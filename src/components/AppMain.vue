8ce4a4a0a079e1204a43296ff97438a7e9cb5cfe
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-03-03 15:13:21 +0100

    Milestone 4 Done

---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js';
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    created() {
+        this.getPosts();
+    },
+    methods: {
+        getPosts() {
+            axios.get(`${this.store.baseUrl}/api/posts`).then((response) => {
+                console.log(response.data.results);
+            })
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style scoped lang="scss">
+
+    @use '../styles/generals.scss' as *;
+
+</style>
